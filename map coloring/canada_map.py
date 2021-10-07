ca2a51adc5af91adc53d25ad94782ffbdadab879
mon-neutrino <monica.sing.2023@gmail.com>
2021-10-07 16:06:48 +0000

    07.10 progress

--- map coloring/canada_map.py
+++ map coloring/canada_map.py
@@ -6,7 +6,40 @@ from hybrid.reference.kerberos import KerberosSampler
 # in each iteration, best results from tabu search and simualted annealing are combined with best results from QPU sample a subproblem
 # https://docs.ocean.dwavesys.com/en/latest/docs_hybrid/reference/reference.html?highlight=kerberos#module-hybrid.reference.kerberos
 
-from utilities import visualize_map
+# from utilities import visualize_map
 
 
+# create class of province
+class Province:
+    def __init__(self, name):
+        self.name = name
+        self.red = name + "_r"
+        self.green = name + "_g"
+        self.blue = name + "_b"
+        self.yellow = name + "_y"
 
+
+# set up province
+bc = Province("bc")   # British Columbia
+ab = Province("ab")   # Alberta
+sk = Province("sk")   # Saskatchewan
+mb = Province("mb")   # Manitoba
+on = Province("on")   # Ontario
+qc = Province("qc")   # Quebec
+nl = Province("nl")   # Newfoundland and Labrador
+nb = Province("nb")   # New Brunswick
+pe = Province("pe")   # Prince Edward Island
+ns = Province("ns")   # Nova Scotia
+yt = Province("yt")   # Yukon
+nt = Province("nt")   # Northwest Territories
+nu = Province("nu")   # Nunavut
+
+# maybe easier way to do ^
+# prov_list = ['bc', 'ab', 'sk', 'mb', 'on', 'qc', 'nl', 'nb', 'pe', 'ns', 'yt', 'nt', 'nu']
+
+# for x,y in zip(prov_list, prov_list):
+#     globals()[x] = Province(y)
+
+provinces = [bc, ab, sk, mb, on, qc, nl, nb, pe, ns, yt, nt, nu]
+
+print(provinces)
